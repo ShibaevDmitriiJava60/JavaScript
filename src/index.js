00fbb4d0ac4b3986c1f228cc6deafb8259e0c71e
dmitrii <dmitriishibaev94@gmail.com>
2025-02-18 19:28:17 +0100

    HW 14

--- src/index.js
+++ src/index.js
@@ -1,48 +1,42 @@
-let array = [3,2,3, 23];
-// //array[1000000] = 10;
-// // console.log(array.length);
-// // console.log(array[0])}
-// function forEach (array, fun) {
-//      for(let i = 0; i < array.length; i++) {
-//         fun(array[i], i);
-//      }
-// }
-// //forEach takes array and fun with two parameters: first - element of array, second - index
-// function print(elem, index){
-//     console.log(`index: ${index}, element: ${elem}`)
-// };
-// forEach(array, print);
+function minMax(...values){
 
+if(Array.isArray(values[0])){
+    arrayValues = [...values[0]];
+    let min=arrayValues[0];
+    let max=arrayValues[0];
+} else {
+    min = values[0];
+    max = values[0];
+}
 
-
-function some(array, fun) {
-    let index = 0;
-    while(index < array.length && !fun(array[index], index)){
-        index++;
+for (let i=1; i<values.length; i++){
+    if(Array.isArray(values[i])){
+        arrayValues = [...values[i]];
+        for(let i=0; i<arrayValues.length; i++){
+            if(arrayValues[i]>=max){
+                max=arrayValues[i];
+            } 
+            else {
+                min=arrayValues[i];
+            } 
+        }
+    } else
+    
+    if(values[i]>=max){
+        max=values[i];
+    } 
+    if(values[i]<min) {
+        min=values[i];
     }
-    return index < array.length;
+}
 
-    //returns true if at least one element of the array match a condition given in the function (fun)
+let res = [min,max];
+return res;
 }
 
 
-
-function evenNumber(num) {
-    return num % 2 == 0;
-}
-console.log(`using "some" function for even numbers array: ${array}, function ${evenNumber}, result: ${some(array, evenNumber)} - false`)
-
-
-function every(array, fun) {
-    let index = 0;
-    while(index < array.length && fun(array[index], index)){
-        index++;
-    }
-    return index == array.length;
-
-}
-array = [2, 2, 4];
-function elmGreaterIndex(elem, index) {
-    return elem > index
-}
-console.log(`using "every" function for elements greater than the index values, array: ${array}, function ${elmGreaterIndex}, result: ${every(array, evenNumber)} - true`)
+console.log(`Minimal and maximal for (1,2,3,4,5,6,7,8) (1,8): ${Math.min(1,2,3,4,5,6,7,8)},${Math.max(1,2,3,4,5,6,7,8)} = ${minMax(1,2,3,4,5,6,7,8)}`);
+console.log(`Minimal and maximal for (1,2,3,4,5,80,-80,-3,-5,-2)(-80,80): ${Math.min(1,2,3,4,5,80,-80,-3,-5,-2)},${Math.max(1,2,3,4,5,80,-80,-3,-5,-2)} = ${minMax(1,2,3,4,5,80,-80,-3,-5,-2)}`); 
+console.log(`Minimal and maximal for (1,2,3,[10,-20,30]) (-20,30): ${Math.min(1,2,3,...[10,-20,30])},${Math.max(1,2,3,...[10,-20,30])} = ${minMax(1,2,3,[10,-20,30])}`);
+console.log(`Minimal and maximal for ([10,-20,30],1,2,3) (-20,30): ${Math.min(...[10,-20,30],1,2,3)},${Math.max(...[10,-20,30],1,2,3)} = ${minMax([10,-20,30],1,2,3)}`);     
+console.log(`Minimal and maximal for (1,2,[10,-20,30],3) (-20,30): ${Math.min(1,2,...[10,-20,30],3)},${Math.max(1,2,...[10,-20,30],3)} = ${minMax(1,2,[10,-20,30],3)}`);   
